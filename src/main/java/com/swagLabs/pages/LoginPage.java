13486ac0cce85fcb994cbf7823c0595a487e6ad3
engRana404 <ranagamaldaif@gmail.com>
2025-05-22 22:34:18 +0300

    Enhance LoginPage with fluent interface and error message assertions for improved login validation

--- src/main/java/com/swagLabs/pages/LoginPage.java
+++ src/main/java/com/swagLabs/pages/LoginPage.java
@@ -1,8 +1,11 @@
 package com.swagLabs.pages;
+
 import com.swagLabs.utils.BrowserActions;
 import com.swagLabs.utils.ElementActions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import org.testng.Assert;
 
 public class LoginPage {
     private final WebDriver driver;
@@ -12,7 +15,7 @@ public class LoginPage {
     private final By username = By.name("username");
     private final By password = By.name("password");
     private final By loginButton = By.id("login-button");
-
+    private final By errorMessage = By.cssSelector("[data-test=\"error\"]");
     //Constructor
     public LoginPage(WebDriver driver) {
         this.driver = driver;
@@ -24,17 +27,33 @@ public class LoginPage {
     }
 
     //Actions > wait > scroll > find > sendKeys
-    public void enterUsername(String username) {
+    public LoginPage enterUsername(String username) {
         ElementActions.sendKeys(driver, this.username, username);
+        return this;
     }
-    public void enterPassword (String password) {
+    public LoginPage enterPassword (String password) {
         ElementActions.sendKeys(driver, this.password, password);
+        return this;
     }
-    public void clickLoginButton() {
+    public LoginPage clickLoginButton() {
         ElementActions.clickElement(driver, loginButton);
+        return this;
+    }
+
+    //Get Error Message
+    public String getErrorMessage() {
+        return ElementActions.getText(driver, errorMessage);
     }
 
     //Validations
+    public LoginPage assertSuccessfulLogin() {
+       Assert.assertEquals(BrowserActions.getCurrentUrl(driver), "https://www.saucedemo.com/inventory.html", "Login Successful");
+        return this;
+    }
 
+    public LoginPage assertUnsuccessfulLogin() {
+        Assert.assertEquals(getErrorMessage(), "Epic sadface: Username and password do not match any user in this service", "Login Unsuccessful");
+        return this;
+    }
 
 }
