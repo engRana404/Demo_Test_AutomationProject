774d3d57ac6e57c7656205b75056ce02586b496b
engRana404 <RanaGamalDaif@gmail.com>
2025-09-03 00:06:48 +0300

    Add Allure step annotations to validation methods for improved reporting

--- src/main/java/com/swagLabs/utils/Validations.java
+++ src/main/java/com/swagLabs/utils/Validations.java
@@ -14,22 +14,27 @@ public class Validations {
     public static void validationTrue(boolean condition, String message) {
         Assert.assertTrue(condition, message);
     }
+
     @Step("Validate condition: {condition} with message: {message}")
     public static void validationFalse(boolean condition, String message) {
         Assert.assertFalse(condition, message);
     }
+
     @Step("Validate actual: {actual} equals expected: {expected} with message: {message}")
     public static void validationEquals(Object actual, Object expected, String message) {
         Assert.assertEquals(actual, expected, message);
     }
+
     @Step("Validate actual: {actual} not equals expected: {expected} with message: {message}")
     public static void validationNotEquals(Object actual, Object expected, String message) {
         Assert.assertNotEquals(actual, expected, message);
     }
+
     @Step("Validate the URL of the page is: {expectedUrl}")
     public static void validatePageURL(WebDriver driver, String expectedUrl) {
         Assert.assertEquals(BrowserActions.getCurrentUrl(driver), expectedUrl, "URL does not match");
     }
+
     @Step("Validate actual: {actual} contains expected: {expected} with message: {message}")
     public static void validatePageTitle(WebDriver driver, String expectedTitle) {
         Assert.assertEquals(BrowserActions.getPageTitle(driver), expectedTitle, "Title does not match");
